8bd65b6c9a72fb431832fb67f60dc9aed9c9e283
Ken Fazzone <kfazz01@gmail.com>
2019-07-07 18:18:28 -0400

    Vecot aliases, ram vector handle

--- demos/various/NIL-ARMCM3-GENERIC/cmparams.h
+++ demos/various/NIL-ARMCM3-GENERIC/cmparams.h
@@ -6,6 +6,17 @@
 #define CORTEX_HAS_FPU          0
 #define CORTEX_PRIORITY_BITS    3
 #define CORTEX_NUM_VECTORS      32
+#define CORTEX_VTOR_INIT __ramvec__
+
+#define USBUART_EP1_Handler Vector40	//0
+#define USBUART_EP2_Handler Vector44	//1
+#define USBUART_EP3_Handler Vector48	//2
+#define USBUART_DP_Handler Vector70	//12
+#define USBUART_Sof_Handler Vector94	//21
+#define USBUART_Arb_Handler Vector98	//22
+#define USBUART_Reset_Handler Vector9C	//23
+#define USBUART_EP0_Handler VectorA0  	//24
+#define USBUART_Ord_Handler VectorA4	//25
 
 #if !defined(_FROM_ASM_)
 #include "core_cm3_psoc5.h"
